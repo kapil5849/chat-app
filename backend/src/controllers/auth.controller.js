14c5d93de6439f8208dc3cf07334d28b98e2077c
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2025-04-27 00:47:40 +0530

    just make protact route and use cloudinary for profile

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs'
+import bcrypt, { hash } from 'bcryptjs'
 import {generateToken} from '../lib/utils.js'
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup = async (req,res) => {
     const {fullName, email, password} = req.body
@@ -21,7 +22,7 @@ export const signup = async (req,res) => {
         const newUser = new User({
             fullName,
             email,
-            password,
+            password: hashedPassword,
         })
         if(newUser){
             // generate jwt token here
@@ -44,12 +45,15 @@ export const signup = async (req,res) => {
 
 export const login = async (req,res) => {
     const {email, password} = req.body;
+    console.log(email, password, "email and password")
     try{
         const user = await User.findOne({email});
+        console.log(user, "user");
         if(!user){
             return res.status(400).json({message: "Invalid credentials"})
         }
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        console.log(isPasswordCorrect, password, user.password, "isPasswordCorrect");
         if(!isPasswordCorrect){
             return res.status(400).json({message: "Invalid credentials here"});
         }
@@ -77,6 +81,31 @@ export const logout = (req,res) => {
     }
 };
 
-export const updateProfile = (req, res) => {
-    
+export const updateProfile = async (req, res) => {
+    try{
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message: "Profile picture is required"});
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updateUser = await User.findByIdAndUpdate(userId, 
+            {profilePic: uploadResponse.secure_url},
+            {new: true}
+        );
+        res.status(200).json(updateProfile);
+    }catch{
+        console.log("error in update profile", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try{
+        res.status(200).json(req.user);
+    }catch(error){
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
 }
