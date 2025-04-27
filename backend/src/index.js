ed1e06816aca4c6e2867d05f4d549a2ecfbc80ff
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2025-04-27 11:07:47 +0530

    message backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import dotenv  from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js'
 import cookieParser from 'cookie-parser';
 const app = express()
@@ -13,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
-
+app.use("/api/message", messageRoutes)
 
 // connectDB();
 app.listen(PORT, () => {
