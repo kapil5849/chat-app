4877fce1fe8c4e79765d9de008545f0f73ee9a0f
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2025-04-28 23:58:02 +0530

    setting page, theme temparary implementation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui';
+import THEMES from './src/constants/index.js'
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
@@ -9,4 +10,41 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+  theme: THEMES,
+    // themes: [
+    //   "light",
+    //   "dark",
+    //   "cupcake",
+    //   "bumblebee",
+    //   "emerald",
+    //   "corporate",
+    //   "synthwave",
+    //   "retro",
+    //   "cyberpunk",
+    //   "valentine",
+    //   "halloween",
+    //   "garden",
+    //   "forest",
+    //   "aqua",
+    //   "lofi",
+    //   "pastel",
+    //   "fantasy",
+    //   "wireframe",
+    //   "black",
+    //   "luxury",
+    //   "dracula",
+    //   "cmyk",
+    //   "autumn",
+    //   "business",
+    //   "acid",
+    //   "lemonade",
+    //   "night",
+    //   "coffee",
+    //   "winter",
+    //   "dim",
+    //   "nord",
+    //   "sunset",
+    // ],
+  }
 }
