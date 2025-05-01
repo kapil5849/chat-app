3f6e06710161d895151785e15cb24f0b73c2d064
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2025-05-02 05:28:43 +0530

    Resolve package.json merge conflict

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-<<<<<<< HEAD
-    baseURL: "http://localhost:5001/api",
-=======
     baseURL: import.meta.env.MODE === 'development' ? "http://localhost:5001/api" : '/api',
->>>>>>> master
     withCredentials: true,
 })
