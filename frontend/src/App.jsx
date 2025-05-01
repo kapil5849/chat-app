b9a31036742cde6724b9c47222d82c4301c2da05
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2025-05-02 04:42:15 +0530

    set frontend in backend like one place

---
+++ frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+
+import ProfilePage from './pages/ProfilePage'
+import { useAuthStore } from './store/useAuthStore'
+import {Loader} from 'lucide-react'
+import { Toaster } from "react-hot-toast"
+import { useThemeStore } from './store/useThemeStore'
+import SettingsPage from "./pages/SettingsPage";
+
+const App = () => {
+  const {authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore();
+  const {theme} = useThemeStore();
+  console.log(onlineUsers)
+  useEffect(() => {
+    checkAuth();
+  },[checkAuth]);
+
+  if(isCheckingAuth && !authUser){
+    return(
+      <div className='flex justify-center items-center h-screen'>
+        <Loader className='size-10 animate-spin'/>
+      </div>
+    )
+  }
+
+  return (
+    <div data-theme={theme}>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={authUser ? <HomePage/> : <Navigate to='/login'/>}/>
+        <Route path='/signup' element={!authUser ? <SignupPage/> : <Navigate to="/"/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
+        <Route path='/settings' element={<SettingsPage/>}/>
+        <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
+        
+      </Routes>
+
+      <Toaster/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,98 @@
+import {create} from 'zustand';
+import {axiosInstance} from "../lib/axios.js"
+import toast from 'react-hot-toast';
+import {io} from 'socket.io-client';
+const BASEURL = import.meta.env.MODE === 'development' ? "http://localhost:5001" : '/';
+
+export const useAuthStore = create((set, get) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIng: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+    onlineUsers: [],
+    socket: null,
+    checkAuth: async () => {
+        try{
+            const response = await axiosInstance.get('/auth/check')
+            set({authUser: response.data})
+            get().connectSocket();
+        }catch(error){
+            console.log("Error in checkAuth:", error)
+            set({authUser: null})
+        }finally{
+            set({isCheckingAuth: false})
+        }
+    },
+    signup: async (data) => {
+        set({isSigningUp: true})
+        try{
+            const response = await axiosInstance.post('/auth/signup',data);
+            set({authUser: response.data});
+            toast.success("Account created successfully")
+            get().connectSocket();
+        }catch(error){
+            toast.error(error.response?.data?.message);
+        }finally{
+            set({isSigningUp: false});
+        }
+    },
+    logout: async () => {
+        try{
+            const response = await axiosInstance.post('/auth/logout');
+            set({authUser: null});
+            toast.success(response.data.message);
+            get().disconnectSocket();
+        }catch(error){
+            toast.error(error.response.data.message);
+        }
+    },
+    login: async (data) => {
+        set({isLoggingIng: true});
+        try{
+            const response = await axiosInstance.post('/auth/login', data);
+            set({authUser: response.data});
+            toast.success("logged in successfully");
+            get().connectSocket();
+        }catch(error){
+            toast.error(error.response?.data?.message);
+        }finally{
+            set({isLoggingIng: false});
+        }
+    },
+    updateProfile: async (data) => {
+        set({isUpdatingProfile: true});
+        try{
+            const response = await axiosInstance.put('/auth/update-profile', data);
+            set({authUser: response.data});
+            toast.success("Profile updated successfully")
+        }catch(error){
+            console.log("error in update profile", error);
+            toast.error(error.response?.data?.message);
+        }finally{
+            set({isUpdatingProfile: false});
+        }
+    },
+    connectSocket: () =>{
+        const {authUser} = get();
+        if(!authUser || get().socket?.connected) return;
+        const socket = io(BASEURL,{
+            query: {
+                userId: authUser._id,
+            }
+        })
+        socket.connect();
+        set({socket : socket});
+        socket.on("getOnlineUsers", (userIds) => {
+            set({ onlineUsers: userIds });
+        });
+    },
+    disconnectSocket: () => {
+        const socket = get().socket;
+        if (socket?.connected) {
+            socket.disconnect();
+            set({ onlineUsers: [], socket: null});
+        }
+    },
+    
+}))
